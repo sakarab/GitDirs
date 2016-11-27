a085f8937a453d23cb59e1518f6e44ae5b04f2e9
Sam <sam@dummy.com>
2016-11-27 23:58:05 +0200

    cosmetic changes

--- Src/GitDirs.cpp
+++ Src/GitDirs.cpp
@@ -14,47 +14,48 @@
 
 CAppModule _Module;
 
-int Run(LPTSTR /*lpstrCmdLine*/ = NULL, int nCmdShow = SW_SHOWDEFAULT)
+int Run( LPTSTR /*lpstrCmdLine*/ = NULL, int nCmdShow = SW_SHOWDEFAULT )
 {
-	CMessageLoop theLoop;
-	_Module.AddMessageLoop(&theLoop);
+    CMessageLoop    theLoop;
 
-	CMainDlg dlgMain;
+    _Module.AddMessageLoop( &theLoop );
 
-	if(dlgMain.Create(NULL) == NULL)
-	{
-		ATLTRACE(_T("Main dialog creation failed!\n"));
-		return 0;
-	}
+    CMainDlg dlgMain;
 
-	dlgMain.ShowWindow(nCmdShow);
+    if ( dlgMain.Create( NULL ) == NULL )
+    {
+        ATLTRACE( _T( "Main dialog creation failed!\n" ) );
+        return 0;
+    }
 
-	int nRet = theLoop.Run();
+    dlgMain.ShowWindow( nCmdShow );
 
-	_Module.RemoveMessageLoop();
-	return nRet;
+    int     nRet = theLoop.Run();
+
+    _Module.RemoveMessageLoop();
+    return nRet;
 }
 
-int WINAPI _tWinMain(HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR lpstrCmdLine, int nCmdShow)
+int WINAPI _tWinMain( HINSTANCE hInstance, HINSTANCE /*hPrevInstance*/, LPTSTR lpstrCmdLine, int nCmdShow )
 {
-	HRESULT hRes = ::CoInitialize(NULL);
-// If you are running on NT 4.0 or higher you can use the following call instead to 
-// make the EXE free threaded. This means that calls come in on a random RPC thread.
-//	HRESULT hRes = ::CoInitializeEx(NULL, COINIT_MULTITHREADED);
-	ATLASSERT(SUCCEEDED(hRes));
+    HRESULT     hRes = ::CoInitialize( NULL );
+    // If you are running on NT 4.0 or higher you can use the following call instead to 
+    // make the EXE free threaded. This means that calls come in on a random RPC thread.
+    //	HRESULT hRes = ::CoInitializeEx(NULL, COINIT_MULTITHREADED);
+    ATLASSERT( SUCCEEDED( hRes ) );
 
-	// this resolves ATL window thunking problem when Microsoft Layer for Unicode (MSLU) is used
-	::DefWindowProc(NULL, 0, 0, 0L);
+    // this resolves ATL window thunking problem when Microsoft Layer for Unicode (MSLU) is used
+    ::DefWindowProc( NULL, 0, 0, 0L );
 
-	AtlInitCommonControls(ICC_BAR_CLASSES);	// add flags to support other controls
+    AtlInitCommonControls( ICC_BAR_CLASSES );	// add flags to support other controls
 
-	hRes = _Module.Init(NULL, hInstance);
-	ATLASSERT(SUCCEEDED(hRes));
+    hRes = _Module.Init( NULL, hInstance );
+    ATLASSERT( SUCCEEDED( hRes ) );
 
-	int nRet = Run(lpstrCmdLine, nCmdShow);
+    int     nRet = Run( lpstrCmdLine, nCmdShow );
 
-	_Module.Term();
-	::CoUninitialize();
+    _Module.Term();
+    ::CoUninitialize();
 
-	return nRet;
+    return nRet;
 }
