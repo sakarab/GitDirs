d733baf637e49e03a0f581f04ac003f7f28c10b7
Sam <sam@dummy.com>
2016-11-27 02:34:37 +0200

    Add main menu

--- Src/resource.h
+++ Src/resource.h
@@ -1,21 +1,22 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by GitDirs.RC
+// Used by GitDirs.rc
 //
-
-
-#define IDD_ABOUTBOX				100
-#define IDR_MAINFRAME				128
-//#define IDR_GITDIRSTYPE	129
-#define IDD_MAINDLG				129
+#define IDD_ABOUTBOX                    100
+#define IDR_MAINFRAME                   128
+#define IDD_MAINDLG                     129
+#define IDR_MENU1                       201
+#define IDR_MENU_MAIN                   201
+#define ID_FILE_ABOUT                   32775
+#define ID_FILE_QUIT                    32776
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE	201
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32775
+#define _APS_NEXT_RESOURCE_VALUE        202
+#define _APS_NEXT_COMMAND_VALUE         32777
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
