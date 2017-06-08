005dc25aa2dbc5bade4d6035ce70bffdc6cade01
Sam <sam@dummy.com>
2017-06-08 21:47:20 +0300

    version

---
+++ version_common_rc.h
@@ -0,0 +1,26 @@
+#ifndef RC_INVOKED
+#pragma once
+#endif
+
+#define RC_TO_STRING_SUB(a)     #a
+#define RC_TO_STRING(a)         RC_TO_STRING_SUB(a)
+
+#define SReleaseDescr   "Release Build "
+#define IMajor          1
+#define IMinor          0
+#define IRelease        0
+#define IBuild          10
+
+#define SMajor          RC_TO_STRING(IMajor)
+#define SMinor          RC_TO_STRING(IMinor)
+#define SRelease        RC_TO_STRING(IRelease)
+#define SBuild          RC_TO_STRING(IBuild)
+
+#define FileVer 	    IMajor,IMinor,IRelease,IBuild
+#define ProductVer 	    IMajor,IMinor,IRelease,IBuild
+#define QFileVer        SMajor "." SMinor "." SRelease "." SBuild
+#define QProductVer     SMajor "." SMinor "." SRelease "." SBuild
+#define QFileVerZ       SMajor "." SMinor "." SRelease "." SBuild "\0"
+#define QProductVerZ    SMajor "." SMinor "." SRelease "." SBuild "\0"
+#define Translation     0x408, 1253
+#define QBuild 		    SReleaseDescr " " SRelease "\0"
