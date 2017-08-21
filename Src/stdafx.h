edfe1cc8a6edc79ab41c284171c5a88f9038590f
marnti <dummy@dummy.com>
2017-08-21 18:46:28 +0300

    disable WinSock-1 / use only WinSock-2

--- Src/stdafx.h
+++ Src/stdafx.h
@@ -34,6 +34,7 @@
 #define _WIN32_WINNT    0x0501
 #define _WIN32_IE       0x0501
 #define _RICHEDIT_VER   0x0500
+#define _WINSOCKAPI_   /* Prevent inclusion of winsock.h in windows.h */
 
 #include <atlbase.h>
 #include <atlapp.h>
