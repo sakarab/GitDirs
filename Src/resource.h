edce0d7231d7ccda83706e3bde773829983a8a95
Sam <sam@dummy.com>
2017-12-21 22:46:27 +0200

    add keyboard accelerator for "Save Data" menu

--- Src/resource.h
+++ Src/resource.h
@@ -49,7 +49,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        212
-#define _APS_NEXT_COMMAND_VALUE         32855
+#define _APS_NEXT_COMMAND_VALUE         32857
 #define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
