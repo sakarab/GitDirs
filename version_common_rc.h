b599d982e6bfddb8a05f862872771f61acfaefa2
Sam <sam@dummy.com>
2017-12-21 22:48:31 +0200

    version 1.1.4.27

--- version_common_rc.h
+++ version_common_rc.h
@@ -9,7 +9,7 @@
 #define IMajor          1
 #define IMinor          1
 #define IRelease        4
-#define IBuild          26
+#define IBuild          27
 
 #define SMajor          RC_TO_STRING(IMajor)
 #define SMinor          RC_TO_STRING(IMinor)
